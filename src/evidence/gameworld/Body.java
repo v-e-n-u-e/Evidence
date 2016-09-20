4197fa3b041b2ebde89208f698b6ace48efe089a
Gina <Gina@10.140.151.173>
2016-09-20 14:45:57 +1200

    Action interface and Item abstract class

---
+++ src/evidence/gameworld/Body.java
@@ -0,0 +1,25 @@
+package evidence.gameworld;
+
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+
+public class Body extends Item {
+
+	public Body(String currentImage, String description) {
+		super(currentImage, description);
+	}
+
+	@Override
+	public void addActions() {
+		super.actions = new ArrayList<Action>();
+		actions.add(new PickUp());
+	}
+
+	@Override
+	public void addImages() {
+		super.images = new ArrayList<ImageIcon>();
+		images.add(new ImageIcon("body.png", "Original dead body"));
+	}
+
+}
---
+++ src/evidence/gameworld/PickUp.java
@@ -0,0 +1,36 @@
+package evidence.gameworld;
+/**
+ * Pick up action
+ * allows player to pickup up an item and put it in their inventory
+ * 
+ * @author Georgina Murphy
+ */
+public class PickUp implements Action {
+
+	private String name = "Pick up";
+	private String description = "Pick up this item and put it in your inventory";
+	
+	
+	/**
+	 * Method to apply the pickup action to the provided item
+	 * 
+	 * @param item - the item the action is being applied to
+	 * @return item - a new item with an updated state
+	 */
+	@Override
+	public Item apply(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getDescription() {
+		return description;
+	}
+
+}
