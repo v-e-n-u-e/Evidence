068da7f8aaaf49363ad6e50dacc9021b9b816022
Connor <mootconnor@gmail.com>
2016-10-08 20:51:43 +1300

    Reads in a whole setup Game without players

--- src/evidence/datastorage/TestReadXml.java
+++ src/evidence/datastorage/TestReadXml.java
@@ -1,28 +1,46 @@
 package evidence.datastorage;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import evidence.gameworld.Game;
+import evidence.gameworld.Player;
 import evidence.gameworld.Room;
-
+import evidence.gameworld.Wall;
+/**
+ * 
+ * @author Connor
+ *
+ */
 public class TestReadXml {
 
 public static void main(String[] args){
+	//Reading in rooms array from game works, havnt tried players.
 		
 		try{
 			File file = new File("SavedGamed.xml");
-			JAXBContext jaxbContext = JAXBContext.newInstance(Room.class);
+			JAXBContext jaxbContext = JAXBContext.newInstance(Game.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			Room room = (Room) jaxbUnmarshaller.unmarshal(file);
-			System.out.println(room.getName().toString());
-			System.out.println(room.getWalls()[0].getItems().get(0));
-			System.out.println(room.getWalls()[0].getDirection().toString());
-			System.out.println(room.getWalls()[0].getX());
-			
-			
+			Game game = (Game) jaxbUnmarshaller.unmarshal(file);
+			List<Player> players = new ArrayList<Player>();
+			List<Room> rooms = new ArrayList<Room>();
+			players = game.getPlayers();
+			rooms = game.getRoom();
+			for(Room r : rooms){
+				System.out.println(r.getName().toString());
+				Wall[] wall = r.getWalls();
+				for(int i=0;i<4;i++){
+					System.out.println(wall[i].getDirection().toString());
+					if(wall[i].getItems().size() != 0){
+						System.out.println(wall[i].getItems().get(0).getDescription());
+					}
+				}
+			}
 			
 		}catch (JAXBException e) {
 		 e.printStackTrace();	
